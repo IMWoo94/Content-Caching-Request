fd1ce97ac1dade5796fa367db34e560deea11d67
IMWoo94 <leemeawoo94@gmail.com>
2024-04-22 18:48:18 +0900

    init : project 환경 구성

---
+++ reuse-request/src/main/java/com/caching/reuserequest/ReuseRequestApplication.java
@@ -0,0 +1,13 @@
+package com.caching.reuserequest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReuseRequestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReuseRequestApplication.class, args);
+	}
+
+}
---
+++ reuse-request/src/test/java/com/caching/reuserequest/ReuseRequestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caching.reuserequest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReuseRequestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
